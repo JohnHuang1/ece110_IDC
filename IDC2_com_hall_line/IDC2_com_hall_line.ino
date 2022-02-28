3142351df8f27399dd6e4f4f1679f5cedf7a5121
John Huang <john.huang@duke.edu>
2022-02-28 16:06:34 -0500

    hall_effect sensor code and IDC2

--- IDC2_com_hall_line/IDC2_com_hall_line.ino
+++ IDC2_com_hall_line/IDC2_com_hall_line.ino
@@ -1,11 +1,28 @@
+/*
+    IDC 2 Line Following, Information Gathering & Communication
+    Contains:
+      ICD Circle line following
+      Working hall-effect sensor with indicator LED
+      Communication using Xbee (push button to send, led indicator when received)
+*/
 # include <Servo.h>
+#define Rx 17
+#define Tx 16
 
 Servo servoLeft; // higher number = counter clockwise
 Servo servoRight; // lower number = clockwise
 
-int QTIMiddle = 51;
-int QTILeft = 49;
-int QTIRight = 53;
+const int QTIMiddle = 51;
+const int QTILeft = 49;
+const int QTIRight = 53;
+
+const int COM_LED_PIN = 7;
+const int COM_BUTTON_PIN = 2;
+
+const int threshold = 5;
+const int noMag = 323;
+const int HALL_ANALOG_PIN = A2;
+const int HALL_LED_PIN = 5;
 
 /*
   -1 : none
@@ -18,29 +35,73 @@ int QTIRight = 53;
 int lastSense = -1;
 
 void setup() {
-  tone(4, 3000, 3000);
   delay(1000);
 
   Serial.begin(9600);
+  Serial2.begin(9600);
 
   servoLeft.attach(12);
   servoRight.attach(11);
 
+  pinMode(COM_LED_PIN, OUTPUT);
+  pinMode(COM_BUTTON_PIN, INPUT);
+
+  pinMode(HALL_ANALOG_PIN, INPUT);
+  pinMode(HALL_LED_PIN, OUTPUT);
 }
 
 void loop() {
   // true = on black line ~200 ish
   // false = not on black line ~30 ish
+  // *** TODO *** create calibration technique
   bool middle = RCTime(QTIMiddle) > 100;
   bool left = RCTime(QTILeft) > 100;
   bool right = RCTime(QTIRight) > 100;
 
-  Serial.print("Middle: ");
-  Serial.println(middle);
-  Serial.print("left: ");
-  Serial.println(left);
-  Serial.print("right: ");
-  Serial.println(right);
+  // Communication button press
+  bool buttonPressed = digitalRead(COM_BUTTON_PIN);
+
+  // Hall-effect sensor analog input
+  int val = analogRead(A2);
+
+  // ** Log QTI sensor data **
+  // Serial.print("Middle: ");
+  // Serial.println(middle);
+  // Serial.print("left: ");
+  // Serial.println(left);
+  // Serial.print("right: ");
+  // Serial.println(right);
+
+  //Check if message should be sent
+  if(Serial.available() || buttonPressed){
+    char outgoing = Serial.read();
+    if(buttonPressed){
+      outgoing = 'f';
+    }
+    Serial.print("Sending: ");
+    Serial.println(outgoing);
+    Serial2.print(outgoing);
+  }
+
+  //Check if there is an incoming message
+  if(Serial2.available()){
+    char incoming = Serial2.read();
+    Serial.print("Receiving: ");
+    Serial.println(incoming);
+    digitalWrite(COM_LED_PIN, HIGH);
+  } else {
+    delay(200);
+    digitalWrite(COM_LED_PIN, LOW);
+  }
+
+  // Hall-effect sensor led indicator 
+  // Serial.println(val); // Log sensor values
+  if(val < noMag - threshold || val > noMag + threshold){
+    digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(5, LOW);
+  }
+
   int time = 1;
   
   if(middle && left && right) {
@@ -55,35 +116,6 @@ void loop() {
     goLeft(50);
     return;
   }
-
-  // if(!middle && !left && !right) {
-  //   // off line
-  //   // lastSense = 3;
-  //   switch(lastSense){
-  //     case 0: // on hash
-  //       goBackwardRight(time);
-  //       break;
-  //     case 1: //on left
-  //       goLeft(time);
-  //       break;
-  //     case 2: //on right
-  //       goRight(time);
-  //       break;
-  //     case 3:  // off line
-  //       goBackward(time);
-  //       break;
-  //     case 4: // on line
-  //       goForwardLeft(time);
-  //       break;
-  //     default:
-  //       stopMoving(time);
-  //       break;
-  //   }
-  //   // goLeft(time);
-  //   return;
-  // }
-
-
 
   if(middle && !left && !right){
     //on line
@@ -119,6 +151,7 @@ void loop() {
     return;
   }
 
+  // off line
   // if(!middle && !left && !right) {
   //   goBackward(50);
   //   return;
@@ -145,7 +178,6 @@ void goBackwardRight(int time){
   servoRight.writeMicroseconds(1600);
   delay(time);
 }
-
 
 void stopMoving(int time){
   // lastMove = 4;
